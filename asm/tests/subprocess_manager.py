20493bd422888b4710682c12e6854ef6acdad7fc
Clémentine Urquizar <clementine.urquizar@gmail.com>
2018-11-01 19:10:16 +0100

    wip regex

--- asm/tests/subprocess_manager.py
+++ asm/tests/subprocess_manager.py
@@ -1,3 +1,4 @@
+import re
 import signal
 from display import Display as d
 from input_tab import INPUT as it
@@ -36,15 +37,22 @@ class SubprocessManager:
         return it[self.filename]['error_line']
 
     def get_output_err_line(self):
+        # m = re.search('*Error*\n', self.output)
+        print("titi")
+        # print(m.group(0))
         return ""
+
+    def print_parsing_test_rslt(self):
+        print("PARSING TEST")
+        print("msg expected:" + self.error_msg_expected())
+        print("line expected:" + str(self.error_line_expected()))
+        self.get_output_err_line()
+
 
     def print_result(self):
         # if this test should return an error = parsing test
         if self.is_parsing_test():
-            print("PARSING TEST")
-            print("msg expected:" + self.error_msg_expected())
-            print("line expected:" + str(self.error_line_expected()))
-            # print_error_test_rslt(output)
+            self.print_parsing_test_rslt()
         else:
             d.print_ok()
             # print_file_test_rslt(self.output)
