9c9fbc881084796e9b4e85f7c7778f4b715d57d2
Clementine Urquizar <clementine.urquizar@gmail.com>
2018-11-09 15:25:39 +0100

    begin read_and_fill

--- vm/src/main.c
+++ vm/src/main.c
@@ -3,29 +3,71 @@
 exit_t	close_fd(int fd)
 {
 	char	*fd_str;
+	char	*err_str;
 
 	if (close(fd) == -1)
 	{
 		fd_str = ft_itoa(fd);
-		perror(fd_str);
+		err_str = ft_strjoin("Close error: ", fd_str);
+		perror(err_str);
 		ft_strdel(&fd_str);
+		ft_strdel(&err_str);
 		return (EXIT_FAILURE);
 	}
 	return (EXIT_SUCCESS);
 }
 
-/*exit_t read_file*/
+exit_t open_file(char *filename)
+{
+	int		fd;
+	char	*err_str;
+
+	if ((fd = open(filename, O_RDONLY)) <= 0)
+	{
+		err_str = ft_strjoin("Open error: ", filename);
+		perror(err_str);
+		ft_strdel(&err_str);
+		return (EXIT_FAILURE);
+	}
+	return (fd);
+}
+
+void	fill_memory(char value)
+{
+	(void)value;
+}
+
+exit_t read_and_fill(char fd, int mem_offset)
+{
+	int		i;
+	char	buff[1];
+	int		read_ret;
+
+	i = mem_offset;
+	while (i < MEM_SIZE)
+	{
+		read_ret = read(fd, buff, 1);
+		if (read_ret == -1)
+		{
+			perror("Read error");
+			return (EXIT_FAILURE);
+		}
+		if (read_ret == 0)
+			break;
+		else
+			fill_memory(buff[0]);
+	}
+	return (EXIT_SUCCESS);
+}
 
 exit_t process_file(char *filename)
 {
 	int		fd;
 
-	if ((fd = open(filename, O_RDONLY)) == -1)
-	{
-		perror(filename);
+	if ((fd = open_file(filename)) == EXIT_FAILURE)
 		return (EXIT_FAILURE);
-	}
 	ft_printf("ouvert :)\n"); // DEBUG
+	read_and_fill(fd, 0);
 	return (close_fd(fd));
 }
 
