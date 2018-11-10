deacc329c130d7b4cda471551d54497de1a945e0
Clementine Urquizar <clementine.urquizar@gmail.com>
2018-11-10 08:11:04 +0100

    upd struct

--- vm/include/vm.h
+++ vm/include/vm.h
@@ -10,17 +10,24 @@
 typedef		uint32_t t_exit;
 typedef		unsigned char t_byte;
 
-# define HEADER_ERR "Error: wrong header format"
+# define HEADER_ERR "Error: wrong header format\n"
 
 typedef struct	s_memcase
 {
-	char	value;
-	char	*color;
+	char		value;
+	char		*color;
 }				t_memcase;
+
+typedef struct	s_player
+{
+	t_header	header;
+	int			num;
+}				t_player;
 
 typedef struct 	s_vm
 {
 	t_memcase	memory[MEM_SIZE];
+	t_player	players[MAX_PLAYERS];
 }				t_vm;
 
 #endif
