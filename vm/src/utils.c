147760aca2af27e5b9fa93ebcec4293361334793
Clementine Urquizar <clementine.urquizar@gmail.com>
2018-12-09 18:24:53 +0100

    add comment about err

--- vm/src/utils.c
+++ vm/src/utils.c
@@ -20,6 +20,8 @@ int		get_mem_index(int index)
 	return (index % (MEM_SIZE - 1));
 }
 
+/*/!\ ne vérifie pas si c'est un T_REG. Si je n'ajoute pas cette condition à la*/
+/*function -> changer le nom en is_valid_reg_index*/
 t_bool	is_valid_reg(int index)
 {
 	return (index > 0 && index <= REG_NUMBER);
