bfbd44747088adfb9e73d5d9811ea19a653a5df6
Clementine Urquizar <clementine.urquizar@gmail.com>
2018-12-13 16:10:49 +0100

    minor reorg

--- vm/src/op_and.c
+++ vm/src/op_and.c
@@ -20,6 +20,7 @@ void	op_and(t_vm *vm, t_processus *proc, t_param *params)
 	rslt = p1 & p2;
 	if (is_valid_reg_index(params[2].value) == TRUE)
 		proc->reg[params[2].value - 1] = rslt;
+	// est ce qu'on modifie le carry si p3 n'est pas un valid index de reg ??
 	proc->carry = (rslt == 0) ? 1 : 0; // pas sûr pour le zero !
 	ft_dprintf(vm->trace_fd, "p1 = 0x%x\n", p1); //DEBUG
 	ft_dprintf(vm->trace_fd, "p2 = 0x%x\n", p2); //DEBUG
