f9514ae0f78f60e0a4fa306135940541ce0b1f56
Sarah Franc <sfranc@student.42.fr>
2019-02-14 10:51:29 +0100

    adding header at last

--- asm/include/lexer.h
+++ asm/include/lexer.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sfranc <sfranc@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/14 10:42:06 by sfranc            #+#    #+#             */
+/*   Updated: 2019/02/14 10:42:52 by sfranc           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef LEXER_H
 # define LEXER_H
 
@@ -6,7 +18,7 @@
 # define STRING_CHARS	LABEL_CHARS"-"
 
 t_ex_ret				lexer(t_token_list **tokens, char *line, int nb_line);
-t_type 					get_token_type(char *token_name);
+t_type					get_token_type(char *token_name);
 t_ex_ret				link_token(t_token_list **tokens, char **token_name,
 							int line, int col);
 void					free_tokens(t_token_list **tokens);
